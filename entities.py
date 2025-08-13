c5e671554156c8a23da00d195c6d7a24e0771646
Ch <chloe.quehan@gmail.com>
2025-08-13 12:17:06 +0200

    class player + draw player method

--- entities.py
+++ entities.py
@@ -1,1 +1,20 @@
 ### Fichier contenant les classes de l'application ###
+
+import pygame
+from config import RED_COLOR, SCREEN_WIDTH, SCREEN_HEIGHT, GROUND_MARGIN
+
+class Player:
+    def __init__(self, width=10, height=20, color=RED_COLOR):
+        self.width = width
+        self.height = height
+        self.color = color
+        # Création du rectangle à la bonne position (x fixe, y ajusté via bottom)
+        self.rect = pygame.Rect(
+            (SCREEN_WIDTH - self.width) // 5, # x, un peu sur la gauche
+            0, # y
+            self.width, # width
+            self.height) # height
+        self.rect.bottom = SCREEN_HEIGHT - GROUND_MARGIN # ajustement du y
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
--- game.py
+++ game.py
@@ -1,20 +1,23 @@
 ### Definition des éléments nécessaire et fonctionnement du jeu ###
 
 import pygame
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, FPS, BLUE_COLOR
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, FPS, BLUE_COLOR, GREEN_COLOR
+from entities import Player
 
 def run_game() :
     pygame.init()
     pygame.display.set_caption(GAME_TITLE)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player()
     
     running = True
     while running :
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 running = False
-        screen.fill(BLUE_COLOR)
+        screen.fill(GREEN_COLOR)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
