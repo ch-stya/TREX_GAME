aa0d7b6afd7b947d2c226a5ad7ffe5e9f5dd6757
ch-stya <chloe.quehan@gmail.com>
2025-08-16 23:44:11 +0200

    class obstacle + collision + game over

--- entities.py
+++ entities.py
@@ -1,14 +1,14 @@
 ### Fichier contenant les classes de l'application ###
 
 import pygame
-from config import RED_COLOR, SCREEN_WIDTH, SCREEN_HEIGHT, GROUND_MARGIN, GROUND_Y
+from config import RED_COLOR, BLUE_COLOR, SCREEN_WIDTH, SCREEN_HEIGHT, GROUND_MARGIN, GROUND_Y
 
 class Player:
     def __init__(self, width=10, height=20, color=RED_COLOR):
         self.width = width
         self.height = height
         self.color = color
-        # Création du rectangle à la bonne position (x fixe, y ajusté via bottom)
+        # Création du rectangle à la bonne position
         self.rect = pygame.Rect(
             (SCREEN_WIDTH - self.width) // 5, # x, un peu sur la gauche
             GROUND_Y - height, # y, sur le sol et en déduisant la taille de l'objet car coord x,y se trouvent en haut à gauche
@@ -36,3 +36,26 @@ class Player:
         if self.rect.bottom >= GROUND_Y :
             self.rect.bottom = GROUND_Y
             self.gravity = 0
+
+class Obstacle:
+    def __init__(self, width=30, height=30, color=BLUE_COLOR, speed=5):
+        self.width = width # largeur
+        self.height = height # hauteur
+        self.color = color # couleur
+        self.speed = speed # vitesse
+        # Création du rectangle à la bonne position (x fixe, y ajusté via bottom)
+        self.rect = pygame.Rect(
+            (SCREEN_WIDTH), # x, tout à droite légérement en dehors de la fenêtre
+            GROUND_Y - height, # y, sur le sol et en déduisant la taille de l'objet car coord x,y se trouvent en haut à gauche
+            self.width, # width
+            self.height) # height
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+    def update(self):
+        # Déplacement de l'obstacle vers la gauche
+        if self.rect.right < 0 :
+            self.rect.x = SCREEN_WIDTH 
+        else :
+            self.rect.x -= self.speed
--- game.py
+++ game.py
@@ -1,30 +1,51 @@
 ### Definition des éléments nécessaire et fonctionnement du jeu ###
 
 import pygame
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, FPS, BLUE_COLOR, GREEN_COLOR
-from entities import Player
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, FPS, BLUE_COLOR, RED_COLOR, GREEN_COLOR
+from entities import Player, Obstacle
 
 def run_game() :
     pygame.init()
     pygame.display.set_caption(GAME_TITLE)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 74)  # None = police par défaut, 74 = taille
+    game_over_text = font.render("GAME OVER", True, RED_COLOR)  # texte rouge
+    game_over_text_rect = game_over_text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
+
     player = Player()
+    obstacle = Obstacle()
     
     running = True
+    game_over = False
+
     while running :
         for event in pygame.event.get():
             # Detection de clic sur la croix pour fermer l'app
             if (event.type == pygame.QUIT):
                 running = False
             # Detection d'une touche pressé
-            elif (event.type == pygame.KEYDOWN):
+            elif (event.type == pygame.KEYDOWN and not game_over):
                 # Detection de la touche espace
                 if event.key == pygame.K_SPACE:
                     player.jump()
+
+        if not game_over : 
+            # update des positions
+            obstacle.update()
+            player.apply_gravity()
+            # test de collision entre joueur et obstacle
+            if player.rect.colliderect(obstacle.rect):
+                game_over = True
+
+        # draw
         screen.fill(GREEN_COLOR)
-        player.apply_gravity()
+        obstacle.draw(screen)
         player.draw(screen)
-        pygame.display.flip()
+
+        if game_over :
+            screen.blit(game_over_text, game_over_text_rect)
+
+        pygame.display.flip() # maj affichage
         clock.tick(FPS)
     pygame.quit()
